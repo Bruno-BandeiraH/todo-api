d060ab742811d2d40123e13f680964052ba12124
Bruno <brunohenrique0665@gmail.com>
2023-11-26 14:49:00 -0300

    CREATE SERVICE

---
+++ src/main/java/com/todolist/todolist/service/TodoService.java
@@ -0,0 +1,42 @@
+package com.todolist.todolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.todolist.todolist.entity.Todo;
+import com.todolist.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    
+    TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    // SERVICES
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("dataCriacao").descending().and(
+            Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Integer id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
